0c6519db1f078c5569fd5b3015198791379fb025
jilesanmi <jilesanmi@parkwayprojects.com>
2020-02-27 01:32:33 +0100

    extracting the model class

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,6 @@ import { User } from './models/User';
 
 
 
-const user = new User({ id: 1, name:'Young money', age:0 });
-
-user.events.on('save', () => {
-  console.log(user);
-})
+const user = User.buildUser({name:'sweet', age:23 });
 
 user.save();
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,8 +1,9 @@
 
+import {Model} from './Model';
+import {Attributes} from './Attributes';
 import { Eventing } from './Eventing';
-import { Sync } from './Sync';
-import { Attributes } from './Attributes';
-import { AxiosResponse } from 'axios';
+import { ApiSync } from './ApiSync';
+
 
 export interface UserProps {
   name?: string;
@@ -12,54 +13,14 @@ export interface UserProps {
 
 const url = 'http://localhost:3000/users';
 
-export class User {
+export class User extends Model<UserProps> {
 
- public events: Eventing = new Eventing();
-
- public sync: Sync<UserProps> = new Sync<UserProps>(url);
-
- public attributes: Attributes<UserProps>
-
- constructor(attrs: UserProps) {
-   this.attributes = new Attributes<UserProps>(attrs);
- }
-
- get on() {
-  return this.events.on
- }
-
- get trigger() {
-   return this.events.trigger
- }
-
- get get() {
-   return this.attributes.get
- }
-
- set(update: UserProps): void {
-   this.attributes.set(update);
-   this.events.trigger('change');
- }
-
- fetch(): void {
-   const id = this.attributes.get('id');
-
-   if(typeof id !== 'number') {
-      throw new Error('Cannot fetch without an id');
-   }
-   this.sync.fetch(id).then((response: AxiosResponse): void => {
-    this.set(response.data);
-   })
- }
-
- save(): void {
-    this.sync.save(this.attributes.getAll())
-    .then((response: AxiosResponse): void => {
-      this.trigger('save')
-    })
-    .catch(() => {
-      this.trigger('error')
-    })
- }
+  static buildUser(attrs: UserProps): User {
+    return new User(
+      new Attributes<UserProps>(attrs),
+      new ApiSync<UserProps>(url),
+      new Eventing()
+    )
+  }
 
 }
